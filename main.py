8432138121fa338a9bd3b1f0da43c78ddabe14e2
Leo Gimenez <leogimenez.dev@gmail.com>
2024-09-30 15:45:30 -0300

    funcion de conexion

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import mysql.connector
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Conectar a la base de datos MySQL en XAMPP
+def conectar_db():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="usuario",        # Usuario de MySQL
+        password="ontraseña", # Contraseña de MySQL
+        database="datos"
+    )
+    return conexion
